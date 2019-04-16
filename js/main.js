784ea4f63d9ffcd10ce2937c903be4d5cb6a085f
Brenda Rangel <brendaorb97@gmail.com>
2019-04-16 01:53:32 -0500

    new
    client reparations

---
+++ js/main.js
@@ -0,0 +1,109 @@
+(function() {
+    'use scrit';
+    document.addEventListener("DOMContentLoaded", function() {
+        var map = L.map('mapa').setView([25.677221, -100.354385], 15);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([25.677221, -100.354385]).addTo(map)
+            .bindPopup('Pancho Birotes')
+            .openPopup();
+    }) // DOM Content Loaded
+})();
+
+$(function() {
+    
+    // $('body').niceScroll({
+    //     cursorwidth:"12px",
+    //     zindex: "3"
+    // });
+    // Lettering
+    //$('.nombre-sitio').lettering();
+    let normalNavHeight = $('.barra').innerHeight();
+    // Menu sigue al usuario
+
+    $(window).scroll(function() {
+        var windowHeight = $(window).height();
+        var navHeight = $('.barra').innerHeight();
+        var scroll = $(window).scrollTop();
+        if (scroll > windowHeight) {
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top': navHeight+'px'});
+        } else {
+            $('.barra').removeClass('fixed');
+            $('body').css({'margin-top': '0px'});
+        }
+    });
+
+    // Menu movil
+    $('.menu-movil').on('click', function() {
+        $('.navegacion-principal').slideToggle();
+        $('.navegacion-principal').css({display : 'inline-block'});
+    })
+
+    // Menu comida
+    $('section.comidas ul:first').show();
+    $('section.comidas a:first').addClass('activo');
+    $('section.comidas a.estiloComida').on('click', function() {
+        $('section.comidas a.estiloComida').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    });
+
+    // Mail to option
+    $('.form-usuario a').on('click', function() {
+        var link = "mailto:panchobirotes1@gmail.com"
+             + "?subject=" + escape($('div.informacion-usuario input')[0].value + " " + $('div.informacion-usuario input')[1].value)
+             + "&body=" + escape($('div.mensaje-usuario textarea')[0].value)
+        window.location.href = link;
+    });
+
+
+    // Colorbox
+    if (window.mobilecheck()) {
+        $('.comidaInfo').colorbox({inline:true, width:"80%"});
+    } else {
+        $('.comidaInfo').colorbox({inline:true, width:"45%"});
+    }
+
+    // Allows the page to be automatically scrolled to the appropriate section when the user clicks
+    // the corresponding button in the navigation bar.
+    $('nav.navegacion-principal > a').on('click', function(event) {
+        event.preventDefault();
+        if (window.mobilecheck()) {
+            $('.navegacion-principal').slideToggle();
+            let section = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(section).offset().top-normalNavHeight
+            }, 1000);
+
+        } else {
+            let navHeight = $('.barra').innerHeight();
+            let section = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(section).offset().top-navHeight
+            }, 1000);
+        }
+    });
+
+});
+
+
+// Tracks the action of resizing the window/browser, so as to disable the style attribute in HTML
+// of the main navigation bar, since this attribute is added when the slideToggle() function is used
+// and messes up some of the CSS rules.
+$(window).resize(function() {
+    if (window.innerWidth >= 768) {
+        $('nav.navegacion-principal').attr('style', '');
+    }
+});
+
+window.mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
